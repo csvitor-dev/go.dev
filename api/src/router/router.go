3bddd423738294af0ad77f917400e6fce62393ef
csvitor-dev <csvitor.dev@gmail.com>
2024-10-29 11:19:46 -0300

    fix: adding routes to router

--- api/src/router/router.go
+++ api/src/router/router.go
@@ -1,11 +1,20 @@
 package router
 
 import (
+	"github.com/csvitor-dev/social-media/api/src/router/routes"
 	"github.com/gorilla/mux"
 )
+
+func Configure(r *mux.Router) {
+	for _, route := range routes.UserRoutes {
+		r.HandleFunc(route.URI, route.Handler).Methods(route.Method)
+	}
+}
 
 // Generate: returns a new HTTP Handler router
 func Generate() *mux.Router {
 	r := mux.NewRouter()
+	Configure(r)
+
 	return r
 }
