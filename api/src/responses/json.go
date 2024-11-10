8446dbeee39d171c52650e5cc6ae4ae51718c7fa
csvitor-dev <csvitor.dev@gmail.com>
2024-11-10 12:42:33 -0300

    fix: making 'Content-Type' header with JSON work

--- api/src/responses/json.go
+++ api/src/responses/json.go
@@ -8,8 +8,8 @@ import (
 
 // JSON: writes an JSON body like response message
 func JSON(w http.ResponseWriter, status int, data any) {
-	w.WriteHeader(status)
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
 
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		log.Fatalln(err)
