204e3dcae7519e0194749120fb824bbe769c8bea
csvitor-dev <csvitor.dev@gmail.com>
2025-08-18 18:43:11 -0300

    feat: create function to validate token received

--- src/api/services/auth/token.go
+++ src/api/services/auth/token.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	"errors"
+	"fmt"
 	"time"
 
 	"github.com/csvitor-dev/social-media/internal/config"
@@ -16,4 +18,24 @@ func CreateToken(userId uint64) (string, error) {
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(config.Env.SECRET_KEY)
+}
+
+func ValidateToken(token string) error {
+	refinedToken, err := jwt.Parse(
+		token,
+		func(t *jwt.Token) (any, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("auth: signing method mismatch: %v", t.Method.Alg())
+			}
+			return config.Env.SECRET_KEY, nil
+		})
+
+	if err != nil {
+		return err
+	}
+
+	if _, ok := refinedToken.Claims.(jwt.MapClaims); !ok || !refinedToken.Valid {
+		return errors.New("auth: invalid token")
+	}
+	return nil
 }
