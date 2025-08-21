552f0c9f69e7d49618e113d395ecb2ea61cc72b1
csvitor-dev <csvitor.dev@gmail.com>
2025-08-21 13:25:36 -0300

    refactor: remove unused variables

--- internal/config/email_env.go
+++ internal/config/email_env.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -10,11 +9,8 @@ import (
 
 // SMTPConfig: SMTP server configuration
 type SMTPEnviroment struct {
-	HOST     string
-	PORT     string
-	EMAIL    string
-	USERNAME string
-	API_KEY  string
+	EMAIL   string
+	API_KEY string
 }
 
 var EmailEnv SMTPEnviroment
@@ -26,13 +22,9 @@ func LoadEmailEnv() {
 		log.Fatal(err)
 	}
 	EmailEnv = SMTPEnviroment{
-		HOST:     os.Getenv("EMAIL_SMTP"),
-		PORT:     os.Getenv("EMAIL_PORT"),
-		EMAIL:    os.Getenv("EMAIL_SENDER"),
-		USERNAME: os.Getenv("EMAIL_USER"),
-		API_KEY:  os.Getenv("RESEND_API_KEY"),
+		EMAIL:   os.Getenv("EMAIL_SENDER"),
+		API_KEY: os.Getenv("RESEND_API_KEY"),
 	}
-	EmailEnv.EMAIL = fmt.Sprintf("%s <%s>", EmailEnv.USERNAME, EmailEnv.EMAIL)
 
 	log.Println("Email enviroment variables loaded successfully!")
 }
