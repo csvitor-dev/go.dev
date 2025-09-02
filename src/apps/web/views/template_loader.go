31c4b81bf42153b41ebb89cf439727729b348bba
csvitor-dev <csvitor.dev@gmail.com>
2025-09-02 11:49:56 -0300

    fix: remove reference to `filepath` because is unnecessary

--- src/apps/web/views/template_loader.go
+++ src/apps/web/views/template_loader.go
@@ -3,16 +3,9 @@ package views
 import (
 	"fmt"
 	"html/template"
-	"path/filepath"
 )
 
 func LoadTemplateFrom(folder, fileName string) (*template.Template, error) {
-	basePath, err := filepath.Abs("./src/apps/web/views")
-
-	if err != nil {
-		return nil, err
-	}
-	pattern := fmt.Sprintf("%s/%s/%s.html", basePath, folder, fileName)
-
+	pattern := fmt.Sprintf("./src/apps/web/views/%s/%s.html", folder, fileName)
 	return template.ParseFiles(pattern)
 }
