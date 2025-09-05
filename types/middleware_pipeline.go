c359eff2b366cd18ae8be86d704ecc1fb0beb8e7
csvitor-dev <csvitor.dev@gmail.com>
2025-09-04 21:02:02 -0300

    chore: update types

--- types/middleware_pipeline.go
+++ types/middleware_pipeline.go
@@ -6,28 +6,19 @@ import (
 )
 
 type MiddlewarePipeline struct {
-	Middlewares []Middleware
+	middlewares []Middleware
 }
 
-func NewPipeline() *MiddlewarePipeline {
+func NewPipeline(queue ...Middleware) *MiddlewarePipeline {
+	slices.Reverse(queue)
+
 	return &MiddlewarePipeline{
-		Middlewares: []Middleware{},
+		middlewares: queue,
 	}
 }
 
-func (m *MiddlewarePipeline) With(queue ...Middleware) *MiddlewarePipeline {
-	slices.Reverse(queue)
-	m.Middlewares = queue
-
-	return m
-}
-
-func (m *MiddlewarePipeline) All() []Middleware {
-	return m.Middlewares
-}
-
 func (m *MiddlewarePipeline) Apply(target http.HandlerFunc) http.HandlerFunc {
-	for _, middleware := range m.Middlewares {
+	for _, middleware := range m.middlewares {
 		target = middleware(target)
 	}
 
